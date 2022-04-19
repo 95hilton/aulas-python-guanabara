47766b5dd8893d556b092d58cc0d9e52c4cdf43f
Hilton Gonçalves <hiltong8@gmail.com>
2022-04-19 17:31:24 -0300

    Corrigido tipo da variavel nota para float

--- ex007.py
+++ ex007.py
@@ -1,5 +1,5 @@
 #Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média
-materia_a = int(input('Digite a nota obtida na Matéria A:'))
-materia_b = int(input('Digite a nota obtida na Matéria B:'))
+materia_a = float(input('Digite a nota obtida na Matéria A:'))
+materia_b = float(input('Digite a nota obtida na Matéria B:'))
 media = (materia_a + materia_b) / 2
 print('A média do aluno foi: {}' .format(media))
