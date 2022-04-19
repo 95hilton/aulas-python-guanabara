df2fd41d8bb8e4d0ddfb50f98825e2de71a6e10c
Hilton Gonçalves <hiltong8@gmail.com>
2022-04-19 17:49:40 -0300

    Resolvido Ex 14. Criado enunciado Ex 15.

---
+++ ex014.py
@@ -0,0 +1,5 @@
+# Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.
+celsius = float(input('Digite a temperatura em em ºC: '))
+farenheit = ((celsius * 9)/5)+32
+
+print('A temperatura em Farenheit é : {}ºF' .format(farenheit))
