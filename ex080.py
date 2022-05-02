d177176d38db6cae48461cc4e91c91ac43c57291
Hilton Gonçalves <hiltong8@gmail.com>
2022-05-02 16:10:56 -0300

    Enunciado exercícios Listas

---
+++ ex080.py
@@ -0,0 +1,2 @@
+'''Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastreos em uma lista, ja na posição correta de inserção (sem usar o sort()).
+No final, mostre a lista ordenada na tela.'''
