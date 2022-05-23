d98019e3bf99421114397dc75ac68901a5eab6a3
95hilton <hiltong8@gmail.com>
2022-05-23 17:09:29 -0300

    Resolução de exercícios

--- ex090.py
+++ ex090.py
@@ -1,1 +1,16 @@
 '''Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário. No final, mostre o conteúdo da estrutura na tela.'''
+
+aluno = {}
+aluno['nome'] = str(input('Digite o nome do aluno: '))
+aluno['media'] = float(input(f'Digite a média de {aluno["nome"]}: '))
+
+if aluno['media'] >= 7:
+    aluno['situacao'] = 'Aprovado'
+elif 5 < aluno['media'] < 7:
+    aluno['situacao'] = 'Em recuperação'
+else:
+    aluno['situacao'] = 'Reprovado'
+
+print(f'Aluno: {aluno["nome"]}'
+      f'\nMédia: {aluno["media"]:.2f}'
+      f'\nSituação: {aluno["situacao"]}')
--- ex091.py
+++ ex091.py
@@ -1,1 +1,19 @@
-'''Crie um programa onde 4 jogadores joguem um dado e tenha resultados aleatórios. Guarde esses resultados em um dicionário. No final, coloque essse dicionário em ordem sabendo que o vencedor tirou o maior número no dado.'''
+'''Crie um programa onde 4 jogadores joguem um dado e tenha resultados aleatórios. Guarde esses resultados em um dicionário.
+No final, coloque essse dicionário em ordem sabendo que o vencedor tirou o maior número no dado.'''
+from operator import itemgetter
+from random import randint
+from time import sleep
+
+resultado = {}
+ranking = []
+print('Valores sorteados: ')
+for c in range(0, 4):
+    resultado[f'jogador{c+1}'] = randint(1, 6)
+    print(f'O jogador{c+1} tirou {resultado[f"jogador{c+1}"]} no dado.')
+    sleep(1)
+
+print('Ranking dos jogadores: ')
+ranking = sorted(resultado.items(), key=itemgetter(1), reverse=True)
+for i, v in enumerate(ranking):
+    print(f'{i+1}ª lugar: {v[0]} com {v[1]}')
+    sleep(1)
