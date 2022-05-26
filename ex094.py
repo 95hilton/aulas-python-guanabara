1da285bd017c911d5ce7d49437a0cb56627f78b5
95hilton <hiltong8@gmail.com>
2022-05-26 20:22:23 -0300

    Execução exercícios Dicionario

--- ex094.py
+++ ex094.py
@@ -1,5 +1,55 @@
 '''Crie um programa que leia nome, sexo e idade de várias pessoas, guardando os dados de cada pessoa em um dicionário e todos os dicionários em uma lista. No final, mostre:
 A) Quantas pessoas foram cadastradas
-B) A média de idade do grupo
+B) A média de nascimento do grupo
 C) Uma lista com todas as mulheres
- D) Uma lista com todas as pessoas com idade acima da média'''
+D) Uma lista com todas as pessoas com idade acima da média'''
+contador = 0
+media = 0
+idades = 0
+pessoa = dict()
+lista = list()
+mulheres = list()
+acima = list()
+
+while True:
+    pessoa['nome'] = str(input('Digite o nome: '))
+
+    while True:
+        pessoa['sexo'] = str(input('Digite o sexo [M/F]: ')).upper().strip()[0]
+        if pessoa['sexo'] in 'MF':
+            break
+        print('Digite apenas M ou F')
+
+    if pessoa['sexo'] == 'F':
+        mulheres.append(pessoa['nome'])
+
+    pessoa['idade'] = int(input('Digite a idade: '))
+    idades += pessoa['idade']
+
+    contador += 1
+    media = idades / contador
+
+    lista.append(pessoa.copy())
+    pessoa.clear()
+
+    while True:
+        continua = str(input('Deseja continuar? [S/N] ')).upper().strip()[0]
+        if continua in 'SN':
+            break
+        print('ERRO! Responda apenas S ou N')
+    if continua == 'N':
+        print(lista)
+        print(f'O grupo tem {len(lista)} pessoas.')
+        print(f'A média de idade é de {media :.1f} anos')
+        print(f'As mulheres cadastradas foram:', end=' ')
+        for m in mulheres:
+            print(m, end=' ')
+        print()
+        print(f'Lista de pessoas com idade acima da média: ')
+        for p in lista:
+            if p['idade'] > media:
+                print(f'Nome: {p["nome"]}'
+                      f'\nSexo:{p["sexo"]}'
+                      f'\nIdade:{p["idade"]}')
+                print('==============')
+        break
--- ex095.py
+++ ex095.py
@@ -1,1 +1,54 @@
-'''Aprimore o desafio 093 para qeu ele funcione com vários jogadores, incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador'''
+'''Aprimore o desafio 093 para que ele funcione com vários jogadores, incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador'''
+
+estatisticas = []
+jogador = {}
+gols = []
+total = 0
+
+continua = ' '
+while True:
+    jogador['nome'] = str(input('Digite o nome do jogador: '))
+    jogador['partidas'] = int(input(f'Digite quantas partidas o jogador {jogador["nome"]} jogou: '))
+
+    for i in range(0, jogador['partidas']):
+        gol = int(input(f'Quantos gols na partida {i+1}? '))
+        gols.append(gol)
+        total += gol
+
+    jogador['gols'] = gols.copy()
+    gols.clear()
+    jogador['total'] = total
+    estatisticas.append(jogador.copy())
+    jogador.clear()
+    total = 0
+    while True:
+        continua = str(input('Deseja continuar? [S/N] ')).upper().strip()[0]
+        if continua in 'SN':
+            break
+        print('ERRO! Digite apenas S ou N.')
+    if continua == 'N':
+        for i in estatisticas[0].keys():
+            print(f'{i:<15}', end='')
+        print()
+        print('=' * 50)
+        for k, v in enumerate(estatisticas):
+            print(f'{k:>4}', end=' ')
+            for i in v:
+                print(f'{str(v[i]):<15}', end=' ')
+            print()
+
+        exibir = int(input('Mostrar dados de qual jogador? (Selecione pelo índice ou 999 para sair) '))
+        while True:
+           if exibir == 999:
+                break
+           elif exibir >= len(estatisticas):
+               print(f'ERRO! Não existe jogador com código {exibir}. Tente novamente.')
+               exibir = int(input('Mostrar dados de qual jogador? (Selecione pelo índice ou 999 para sair) '))
+           else:
+               print(f'---Levantamento do jogador {estatisticas[exibir]["nome"]}')
+               for k, v in enumerate(estatisticas[exibir]["gols"]):
+                    print(f'No jogo {k+1} fez {v} gols')
+
+               exibir = int(input('Mostrar dados de qual jogador? (Selecione pelo índice ou 999 para sair) '))
+
+        break
