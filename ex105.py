2b70e4a70c8781adc2b1a6b6fcc61f0950aa8012
95hilton <hiltong8@gmail.com>
2022-05-31 17:05:03 -0300

    Complemento Exercicios Funções

--- ex105.py
+++ ex105.py
@@ -8,3 +8,47 @@
 
 Adicione também as docstrings da função.'''
 
+
+def notas(*n, sit=False):
+    """
+    ## Função para analisar notas e situação de vários alunos.
+    :param n: recebe uma ou mais notas dos alunos
+    :param sit: valor opcional, define se o programa irá exibir ou não a situação do aluno
+    :return: retorna um dicionario com várias informações sobre o aluno
+    """
+    r = dict()
+    cont = 0
+    maior = 0
+    menor = 0
+    soma = 0
+
+    while cont < len(n):
+        if cont == 0:
+            menor = n[cont]
+        if n[cont] > maior:
+            maior = n[cont]
+        if n[cont] < menor:
+            menor = n[cont]
+        print(n[cont])
+        soma += n[cont]
+        cont += 1
+
+    media = soma / cont
+    r['total'] = cont
+    r['maior'] = maior
+    r['menor'] = menor
+    r['media'] = f'{media:.3}'
+
+    if sit:
+        if media <= 5:
+            r['situacao'] = 'RUIM'
+        elif 5 < media <= 7:
+            r['situacao'] = 'RAZOÁVEL'
+        else:
+            r['situacao'] = 'ÓTIMA'
+    return r
+
+
+resp = notas(5.5, 2.5, 6.5, 1.2, sit=True)
+print(resp)
+help(notas)
--- ex106.py
+++ ex106.py
@@ -1,2 +1,32 @@
 '''Faça um mini-sistema que utilize o interactive Help do Python. O usuário vai digitar o comando e o manual vai aparecer. Quando o usuário digitar a palavra "FIM", o programa se encerrará
 OBS: Use cores'''
+
+
+def banner(cor, texto=' '):
+    if cor == 'verde':
+        print(f'\033[1;42m{texto}\033[m')
+    elif cor == 'azul':
+        print(f'\033[1;44m{texto}\033[m')
+    elif cor == 'branco':
+        print(f'\033[1;107m	\033[1;30m{texto}')
+    elif cor == 'vermelho':
+        print(f'\033[1;41m{texto}\033[m')
+    elif cor == 'nulo':
+        print('\033[m')
+
+
+def pyHelp():
+    banner('verde', 'SISTEMA DE AJUDA PYHELP')
+    while True:
+        entrada = input('Digite a função ou biblioteca: ').upper().strip()
+        if entrada == 'FIM':
+            banner('vermelho', 'ATÉ LOGO!')
+            break
+        else:
+            banner('azul', f'Acessando o manual do comando \'{entrada}\'...')
+            banner('branco')
+            help(entrada)
+            banner('nulo')
+
+
+pyHelp()
